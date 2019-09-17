95c676d3c0d4a5ee18e19601ba1ebb77b8083466
alexanderhale <a.hale@live.ca>
2019-09-17 17:06:41 -0400

    Assignment 1 specified files

---
+++ Assignment1/proj1_data_loading.py
@@ -0,0 +1,19 @@
+import json # we need to use the JSON package to load the data, since the data is stored in JSON format
+
+with open("proj1_data.json") as fp:
+    data = json.load(fp)
+    
+# Now the data is loaded.
+# It a list of data points, where each datapoint is a dictionary with the following attributes:
+# popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
+# children : the number of replies to this comment (type: int)
+# text : the text of this comment (type: string)
+# controversiality : a score for how "controversial" this comment is (automatically computed by Reddit)
+# is_root : if True, then this comment is a direct reply to a post; if False, this is a direct reply to another comment 
+
+# Example:
+data_point = data[0] # select the first data point in the dataset
+
+# Now we print all the information about this datapoint
+for info_name, info_value in data_point.items():
+    print(info_name + " : " + str(info_value))
