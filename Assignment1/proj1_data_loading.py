a4763a308fb261a8b5ede7749118773d84b0788b
alexanderhale <a.hale@live.ca>
2019-09-18 10:36:47 -0400

    added calculation of x_counts to preprocessing

--- Assignment1/proj1_data_loading.py
+++ Assignment1/proj1_data_loading.py
@@ -2,42 +2,44 @@ import json # we need to use the JSON package to load the data, since the data i
 import re 	# import regex to use when splitting
 from collections import Counter
 
-most_common_words = []
-
-# pre-processing function to gather information from incoming data set
-def preprocess(dictionaries):
-	allWords = []																			# variable to hold overall list of all words
-	for dictionary in dictionaries:															# iterate through each of the supplied comments
-		dictionary['lowercase_text'] = dictionary['text'].lower()							# convert all text to lowercase
-		dictionary['words'] = re.findall(r"[\w']+|[.,!?;]", dictionary['lowercase_text'])	# split text into individual words
-		allWords.extend(dictionary['words'])												# add to overall list of words
-	
-	# count most common words in the comments
-	counter = Counter(allWords)
-	most_common_words = counter.most_common(160)
-	print(most_common_words)						# TODO remove
-
-	# # determine x_counts for each of the comments
-	# for dictionary in dictionaries:
-	# 	i = 0
-	# 	dictionary['x_counts'] = [0] * 160
-	# 	for entry in most_common_words:
-	# 		print('entry : ' + entry[0])
-	# 		single_comment_counter = Counter(dictionary['words'])
-	# 		dictionary['x_counts'][i] = single_comment_counter[entry]
-	# 		i = i + 1
-
-with open("proj1_data.json") as fp:
-    data = json.load(fp)
-    
-# Now the data is loaded.
-# It a list of data points, where each datapoint is a dictionary with the following attributes:
+# COMMENT ATTRIBUTES
+# The "data" variable is a list of data points, where each datapoint is a dictionary with the following attributes:
 # popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
 # children : the number of replies to this comment (type: int)
 # text : the text of this comment (type: string)
 # controversiality : a score for how "controversial" this comment is (automatically computed by Reddit)
 # is_root : if True, then this comment is a direct reply to a post; if False, this is a direct reply to another comment 
+# lowercase_text : the "text" attributed converted to lowercase (can be removed if the "words" attribute is adequate)
+# words : the words of the "lowercase_text" attribute in lowercase, split into elements of a list
+# x_counts : holds at position i the number of times that the word at most_common_words[i][0] occurs in this comment
 
+most_common_words = []	# a dictionary containing the 160 most common words in the data set, as well as the number of times they appear
+						# sorted in descending order of number of occurences
+
+# pre-processing function to gather information from incoming data set
+def preprocess(comments):
+	allWords = []																			# variable to hold overall list of all words
+	for comment in comments:																# iterate through each of the supplied comments
+		comment['lowercase_text'] = comment['text'].lower()									# convert all text to lowercase
+		comment['words'] = re.findall(r"[\w']+|[.,!?;]", comment['lowercase_text'])			# split text into individual words
+		allWords.extend(comment['words'])													# add to overall list of words
+	
+	# count most common words in the comments
+	counter = Counter(allWords)
+	most_common_words = counter.most_common(160)
+
+	# determine x_counts for each of the comments
+	for comment in comments:
+		i = 0
+		comment['x_counts'] = [0]*160
+		for entry in most_common_words:
+			comment_counter = Counter(comment['words'])
+			comment['x_counts'][i] = comment_counter[entry[0]]
+			i = i + 1
+
+with open("proj1_data.json") as fp:			# load data from file
+    data = json.load(fp)
+    
 preprocess(data)	# call the preprocessing function
 
 # Example:
