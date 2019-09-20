4d54c0a7c3f411a9723f9e03a979273089850169
alexanderhale <a.hale@live.ca>
2019-09-20 15:12:28 -0400

    wine data input and preprocessing

---
+++ wine/wine.py
@@ -0,0 +1,29 @@
+import csv
+import numpy as np
+
+# column titles:
+    # "fixed acidity";
+    # "volatile acidity";
+    # "citric acid";
+    # "residual sugar";
+    # "chlorides";
+    # "free sulfur dioxide";
+    # "total sulfur dioxide";
+    # "density";
+    # "pH";
+    # "sulphates";
+    # "alcohol";
+    # "quality"
+
+with open('winequality-red.csv', 'r') as inputfile:
+    winedata = list(csv.reader(inputfile, delimiter=';'))
+
+# convert data to NumPy 2D array
+winedata = np.array(winedata[1:], dtype=np.float)
+
+# turn "quality" attribute to binary
+for row in winedata:
+    if row[11] >= 6:
+        row[11] = 1
+    else:
+        row[11] = 0
