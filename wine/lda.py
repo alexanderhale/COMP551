1f953e7159ff55c2f0969b4a01b89d00ef5f0537
alexanderhale <a.hale@live.ca>
2019-09-26 19:00:56 -0400

    finished LDA implementation; added evaluate_acc method

--- wine/lda.py
+++ wine/lda.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 # Linear Discriminant Analysis
-class lda:
+class LDA:
+    def __init__(self, w0, w):
+        self.w0 = w0     # weight parameter 0
+        self.w = w       # initialized as zero, but later stores a vector of other weights
+
     # x = 2D array containing training data
     # y = results of that training data
-    def fit(x, y):
-        # def __init__():
-
+    # doesn't returning anything, but finishes by modifying model parameters w0 and w
+    def fit(self, x, y):
         # calculate P(y = 0) and P(y = 1)
         n0 = 0                              # stores number of results in class 0
         for datapoint in y:
@@ -27,30 +30,35 @@ class lda:
         mu1 = np.true_divide(mu1, (y.size - n0))    # stores matrix of mean of features when result is class 1
 
         # calculate sigma
-        sigma = np.cov(x)                       # stores the covariance matrix of x (# TODO should this be done manually, or is using np.cov ok?)
-        sigma_inverse = np.linalg.inv(sigma)    # TODO fix the singular matrix error that is coming up here because sigma is a square matrix
+        # TODO calculate this with the formula in the slides instead of the built-in NumPy covariance function
+        sigma = np.cov(x, rowvar=False)              # stores the covariance matrix of x
+        sigma_inverse = np.linalg.inv(sigma)
 
         # calculate log-odds result
-        part1 = np.log(np.true_divide(p1, p0))
+        self.w0 = np.log(np.true_divide(p1, p0))
 
         part2 = np.transpose(mu1)
         part2 = np.dot(part2, sigma_inverse)
         part2 = np.dot(part2, mu1)
-        part2 = np.true_divide(part2, 2)
+        self.w0 -= np.true_divide(part2, 2)
 
         part3 = np.transpose(mu0)
         part3 = np.dot(part3, sigma_inverse)
         part3 = np.dot(part3, mu0)
-        part3 = np.true_divide(part3, 2)
+        self.w0 += np.true_divide(part3, 2)              # w0 stores weight parameter w_0
 
-        part4 = np.transpose(x)
-        part4 = np.dot(part4, sigma_inverse)
-        part4_1 = np.subtract(mu1, mu0)
-        part4 = np.dot(part4, part4_1)
+        self.w = np.subtract(mu1, mu0)
+        self.w = np.dot(sigma_inverse, self.w)           # w stores the vector of other weight parameters
 
-        result = np.subtract(part1, part2)
-        result = np.add(result, part3)
-        result = np.add(result, part4)
-
-        print(result)
-    # def predict():
+    # x = 2D array containing input points upon which predictions will be made
+    # return: vector y^, containing binary predictions about whether each input point is class 0 or 1
+        # y^[0] is prediction for x's first input point row, y^[1] the second input point, etc
+    def predict(self, x):
+        y = np.zeros(x.shape[0])
+        for i in range(y.shape[0]):
+            # find the probability of each data point being in class 1:
+            #   multiply each input data point by the stored weight parameter, and add in bias term
+                # TODO figure out why this probability is not landing between 0 and 1
+            if self.w0 + np.dot(x[i].T, self.w) > 0.5:
+                y[i] = 1        # if probability > 0.5, predict class 1
+        return y
--- wine/wine.py
+++ wine/wine.py
@@ -16,6 +16,19 @@ import lda
     # "alcohol";
     # "quality"
 
+# y = true labels
+# y_hat = training labels
+# return: accuracy of training labels (in percentage)
+# Ensure that y and y_hat contain the labels for the same training examples.
+def evaluate_acc (x, y, y_hat):
+    # TODO input x is not really needed to evaluate accuracy, but project instructions specify it
+    score = 0
+    for i in range(y.shape[0]):
+        if y[i] == y_hat[i]:
+            score += 1
+    return (score / y.shape[0]) * 100
+
+# import data from CSV file
 with open('winequality-red.csv', 'r') as inputfile:
     winedata = list(csv.reader(inputfile, delimiter=';'))
 
@@ -29,4 +42,7 @@ for row in winedata:
     else:
         row[11] = 0
 
-lda.lda.fit(winedata[:, 0:10], winedata[:, 11]) # call LDA with the training data
+wine = lda.LDA(0, 0)                            # create an LDA object
+wine.fit(winedata[:, 0:10], winedata[:, 11])    # call LDA with the training data
+y_hat = wine.predict(winedata[:, 0:10])         # predict the class of all the input data points
+print(evaluate_acc(winedata[:, 0:10], winedata[:, 11], y_hat))  # find out the accuracy of the predictions
